abcc3653e1f5bb6d7e9bb5e5d0107e8aa27624f4
Josh Larsen <2565382+joshlarsen@users.noreply.github.com>
2022-11-30 18:19:25 -0500

    update

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -10,7 +10,7 @@ import {
   GitHubIcon,
   LinkedInIcon,
 } from '@/components/SocialIcons'
-import portraitImage from '@/images/portrait.jpg'
+import portraitImage from '@/images/photos/founders.jpeg'
 
 function SocialLink({ className, href, children, icon: Icon }) {
   return (
@@ -66,53 +66,65 @@ export default function About() {
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
+                Picture this: February in Southern California. The weather is beautiful (isn’t it always?), spirits are high, and mouths are on fire at a local Mexican restaurant. 
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                The first time Greg Martin, Josh Larsen, and Eric Cornelius met in person was arguably the perfect setting to make magic happen. Each of them have spent years in the security space as serial entrepreneurs - building technology and teams from the ground up with the shared goal of creating solutions that solve problems better. Essentially, making security less painful without sacrificing efficacy. The trio was ready to do it again. This time, focusing their sights on securing something that is growing at rates that nobody can keep up with: modern applications and the infrastructure that supports them. 
               </p>
               <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
+                How exactly this could be done was a discussion that could only be had over beer, tacos, and ghost peppers. 
+              </p>
+              <p><em>Challenge the Status Quo</em></p>
+              <p>
+                We live in a digital world. An overplayed sentence, yes, but true nonetheless. 
               </p>
               <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
+                With quite literally the majority of the world relying on applications for both communication and entertainment, ensuring that everyone - not just global 2000 organizations and the government - are protected as they go about their lives each day is top of mind for the Ghost founding trio. 
+              </p>
+              <p>
+                As the years go on, the industry is bursting at the seams with point solutions that are purpose-built to solve a particular part of the problem. This is always painstakingly apparent at industry conferences like Black Hat and RSA where the show floor somehow manages to always get bigger. Although it’s great to see growth, it’s a tough time to be a consumer. It’s hard to tell where one solution stops and another starts.
+              </p>
+              <p>
+              For some reason, we’ve come to accept this. Despite the ‘single pane of glass’ promises made by companies everywhere, we rely on disparate technologies that lead to significant overlap in capabilities, a workforce that’s tired of being expected to understand a myriad of different technologies, and immense budget inefficiencies because of what’s required to bring risk down to an acceptable level. 
+              </p>
+              <p>
+              When it comes to application security, Ghost is going to change that. 
+              </p>
+              <p><em>Enter: Ghost Security</em></p>
+              <p>
+              The application security space is already getting pretty busy. By busy, we mean that sooner or later, just like the rest of the industry, there’s going to be significant bloat and an onus on the user to understand how to pick and choose vendors in order to implement the best solution for them. 
+              </p>
+              <p>
+              Most of today’s approaches to application security are quickly losing effectiveness because they miss addressing the nuances created by modern cloud infrastructure. They rely on the user for continuous inventorying of assets, they don’t leverage machine learning in a forward thinking way, and they certainly aren’t equipped to scale at the rate demanded by today’s cloud scale microservice-driven architectures. 
+              </p>
+              <p>
+              This is where there’s room for change. More than change, there’s room to completely flip what’s acceptable on its head and build a comprehensive, easy to implement, effective solution to protect users and protect the data that they are sharing. 
+              </p>
+              <p>
+              This is what Greg, Eric, Josh, and the entire team at Ghost Security are dedicated to doing.
               </p>
             </div>
           </div>
           <div className="lg:pl-20">
             <ul role="list">
               <SocialLink href="#" icon={TwitterIcon}>
-                Follow on Twitter
+                Ghost on Twitter
               </SocialLink>
               <SocialLink href="#" icon={InstagramIcon} className="mt-4">
-                Follow on Instagram
+                Ghost on Instagram
               </SocialLink>
               <SocialLink href="#" icon={GitHubIcon} className="mt-4">
-                Follow on GitHub
+                Ghost on GitHub
               </SocialLink>
               <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
-                Follow on LinkedIn
+                Ghost on LinkedIn
               </SocialLink>
               <SocialLink
-                href="mailto:spencer@planetaria.tech"
+                href="mailto:boo@ghost[dot]security"
                 icon={MailIcon}
                 className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
               >
-                spencer@planetaria.tech
+                boo@ghost.security
               </SocialLink>
             </ul>
           </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -249,43 +249,42 @@ export default function Home({ articles }) {
     <>
       <Head>
         <title>
-          Spencer Sharp - Software designer, founder, and amateur astronaut
+          Ghost Security - The modern application security platform
         </title>
         <meta
           name="description"
-          content="I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
+          content="Ghost is a venture backed, product-led startup building the new standard in application security for the modern enterprise."
         />
       </Head>
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            The modern application security platform.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            The Ghost Platform is built from the ground up to enable enterprises to bring contextually 
+            relevant security insights to their modern applications at cloud scale and DevOps speed. Stop chasing 
+            false positives, vague alerts, and mountains of useless findings.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
-              href="https://twitter.com"
-              aria-label="Follow on Twitter"
+              href="https://twitter.com/ghostsecurityhq"
+              aria-label="Follow Ghost on Twitter"
               icon={TwitterIcon}
             />
             <SocialLink
-              href="https://instagram.com"
-              aria-label="Follow on Instagram"
+              href="https://instagram.com/ghostsecurityhq"
+              aria-label="Follow Ghost on Instagram"
               icon={InstagramIcon}
             />
             <SocialLink
-              href="https://github.com"
-              aria-label="Follow on GitHub"
+              href="https://github.com/ghostsecurity"
+              aria-label="Follow Ghost on GitHub"
               icon={GitHubIcon}
             />
             <SocialLink
-              href="https://linkedin.com"
-              aria-label="Follow on LinkedIn"
+              href="https://www.linkedin.com/company/ghostsecurity/"
+              aria-label="Follow Ghost on LinkedIn"
               icon={LinkedInIcon}
             />
           </div>
