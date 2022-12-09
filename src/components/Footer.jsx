135992dadeb7e773cf094194b540ced1d50f46bd
Josh Larsen <2565382+joshlarsen@users.noreply.github.com>
2022-12-09 11:43:44 -0500

    re-order nav items

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,11 +21,11 @@ export function Footer() {
           <Container.Inner>
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-                <NavLink href="/about">About</NavLink>
-                <NavLink href="/blog">Blog</NavLink>
                 <NavLink href="/team">Team</NavLink>
-                <NavLink href="/careers">Careers</NavLink>
                 <NavLink href="/platform">Platform</NavLink>
+                <NavLink href="/blog">Blog</NavLink>
+                <NavLink href="/careers">Careers</NavLink>
+                <NavLink href="/about">About</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
                 &copy; {new Date().getFullYear()} Ghost Security, Inc. All
