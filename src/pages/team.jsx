cf0ad33ef5d16de8f8e89d3eea34076abec2a87e
Josh Larsen <2565382+joshlarsen@users.noreply.github.com>
2022-12-09 18:30:25 -0500

    linkedin links

--- src/pages/team.jsx
+++ src/pages/team.jsx
@@ -34,6 +34,56 @@ import { useEffect } from 'react'
 
 let sliderSetupTimeout, sliderTimeout, removerTimeout
 
+const linkedInLinks = {
+  brad: {
+    twitter: 'https://twitter.com/bradgeesaman',
+    linkedIn: 'https://www.linkedin.com/in/bradgeesaman/',
+  },
+  david: { twitter: '#', linkedIn: 'https://www.linkedin.com/in/matslofva/' },
+  don: {
+    twitter: '#',
+    linkedIn: 'https://www.linkedin.com/in/don-campbell79/',
+  },
+  eric: {
+    twitter: '#',
+    linkedIn: 'https://www.linkedin.com/in/eric-cornelius-7b08b85/',
+  },
+  greg: {
+    twitter: 'https://twitter.com/gregcmartin',
+    linkedIn: 'https://www.linkedin.com/in/gregcmartin/',
+  },
+  jack: { twitter: '#', linkedIn: 'https://www.linkedin.com/in/jackwhall/' },
+  jay: { twitter: '#', linkedIn: '#' },
+  jen: { twitter: '#', linkedIn: 'https://www.linkedin.com/in/moremorioka/' },
+  joel: {
+    twitter: 'https://twitter.com/joelschopp',
+    linkedIn: 'https://www.linkedin.com/in/schopp/',
+  },
+  josh: {
+    twitter: 'https://twitter.com/josh_larsen',
+    linkedIn: 'https://www.linkedin.com/in/joshlarsen/',
+  },
+  luis: {
+    twitter: '#',
+    linkedIn: 'https://www.linkedin.com/in/luis-felipe-nicolau',
+  },
+  megan: {
+    twitter: 'https://twitter.com/meganHorner00',
+    linkedIn: 'https://www.linkedin.com/in/meganhorner/',
+  },
+  stevie: { twitter: '#', linkedIn: 'https://www.linkedin.com/in/slpocina/' },
+  yuri: {
+    twitter: '#',
+    linkedIn: 'https://www.linkedin.com/in/yuri-west-b643b9aa/',
+  },
+  chuck: {
+    twitter: '#',
+    linkedIn: 'https://www.linkedin.com/in/chuck-hall-75093726',
+  },
+  sean: { twitter: '#', linkedIn: 'https://www.linkedin.com/in/macdonsp' },
+  owen: { twitter: '#', linkedIn: 'https://www.linkedin.com/in/owenrumney' },
+}
+
 function Article({ article }) {
   return (
     <Card as="article">
@@ -151,7 +201,7 @@ function Photos() {
           <div
             key={`${image.src}${idx}`}
             className={clsx(
-              'relative aspect-[2/3] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 duration-1000 dark:bg-zinc-800 sm:w-56 sm:rounded-2xl',
+              'group relative aspect-[2/3] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 duration-1000 dark:bg-zinc-800 sm:w-56 sm:rounded-2xl',
               rotations[team.indexOf(image)]
             )}
           >
@@ -159,8 +209,27 @@ function Photos() {
               src={image}
               alt=""
               sizes="(min-width: 640px) 18rem, 11rem"
-              className="absolute inset-0 h-full w-full object-cover grayscale hover:grayscale-0"
+              className="absolute inset-0 h-full w-full object-cover grayscale group-hover:grayscale-0"
             />
+            <div className="absolute bottom-3 right-3 m-0.5 hidden h-6 w-6 rounded-md bg-zinc-800 group-hover:block">
+              <a
+                target="blank_"
+                href={
+                  linkedInLinks[image.src.split('.')[0].split('/')[4]].linkedIn
+                }
+              >
+                <svg
+                  fill="currentColor"
+                  className="text-violet-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 26 26"
+                  width="26px"
+                  height="26px"
+                >
+                  <path d="M 21.125 0 L 4.875 0 C 2.183594 0 0 2.183594 0 4.875 L 0 21.125 C 0 23.816406 2.183594 26 4.875 26 L 21.125 26 C 23.816406 26 26 23.816406 26 21.125 L 26 4.875 C 26 2.183594 23.816406 0 21.125 0 Z M 8.039063 22.070313 L 4 22.070313 L 3.976563 9.976563 L 8.015625 9.976563 Z M 5.917969 8.394531 L 5.894531 8.394531 C 4.574219 8.394531 3.722656 7.484375 3.722656 6.351563 C 3.722656 5.191406 4.601563 4.3125 5.945313 4.3125 C 7.289063 4.3125 8.113281 5.191406 8.140625 6.351563 C 8.140625 7.484375 7.285156 8.394531 5.917969 8.394531 Z M 22.042969 22.070313 L 17.96875 22.070313 L 17.96875 15.5 C 17.96875 13.910156 17.546875 12.828125 16.125 12.828125 C 15.039063 12.828125 14.453125 13.558594 14.171875 14.265625 C 14.066406 14.519531 14.039063 14.867188 14.039063 15.222656 L 14.039063 22.070313 L 9.945313 22.070313 L 9.921875 9.976563 L 14.015625 9.976563 L 14.039063 11.683594 C 14.5625 10.875 15.433594 9.730469 17.519531 9.730469 C 20.105469 9.730469 22.039063 11.417969 22.039063 15.046875 L 22.039063 22.070313 Z" />
+                </svg>
+              </a>
+            </div>
           </div>
         ))}
       </div>
