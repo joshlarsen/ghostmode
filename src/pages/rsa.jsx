f28976259c785e667de9f1e8c594021a09742106
Josh Larsen <2565382+joshlarsen@users.noreply.github.com>
2023-01-23 11:20:27 -0500

    re-add 2023

--- src/pages/rsa.jsx
+++ src/pages/rsa.jsx
@@ -124,7 +124,7 @@ export default function LandingPage() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              Hottest 🔥 Security Innovators
+              2023 Hottest 🔥 Security Innovators
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
